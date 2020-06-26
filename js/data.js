b2e2191bdaa6da37800cf6cebef643d6269af38d
Francisco Cajlon Jhonathan Moura Batista <63024734+cajlonbatista@users.noreply.github.com>
2020-06-26 20:51:58 -0300

    Adicionando arquivos

---
+++ js/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    title:
+      "Rap do Luffy  (One Piece) RapTributo 06",
+    artist: "Player Tauz",
+    cover: "Lucy.png",
+    file: "Rap do Luffy  (One Piece)   Tauz RapTributo 06.mp3"
+  },
+  {
+    title:
+      "Rap do Sabo (One Piece)   O Revolucionário ",
+    artist: "Daarui",
+    cover: "Sabo.png",
+    file: "Rap do Sabo (One Piece)   O Revolucionário   Daarui.mp3"
+  },
+  {
+    title:
+      "Rap do Gon (Hunter x Hunter) RapTributo 46",
+    artist: "Player Tauz",
+    cover: "Gon.png",
+    file: "Rap do Gon (Hunter x Hunter)   Tauz RapTributo 46.mp3"
+  },
+  {
+    title:
+      "Rap do Sanji (One Piece)   Tauz RapTributo 37",
+    artist: "Player Tauz",
+    cover: "Sanji.png",
+    file: "Rap do Sanji (One Piece)   Tauz RapTributo 37.mp3"
+  },
+];
